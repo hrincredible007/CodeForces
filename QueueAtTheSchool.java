dead2da9f03e59a4330f274efd98f09bc953f734
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-04-24 00:09:50 +0530

    Queue At The School.

---
+++ QueueAtTheSchool.java
@@ -0,0 +1,37 @@
+package A2OJ.Division_A;
+
+import java.util.Scanner;
+
+public class QueueAtTheSchool {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, t;
+        n = sc.nextInt();
+        t = sc.nextInt();
+
+
+        String str = sc.next();
+        String wd = "";
+        int flag = 0;
+        char[] arr =new char[str.length()];
+        arr = str.toCharArray();
+
+        for(int i =0; i < n-1; i++){
+            if(t-- >0) {
+                for (int j = 0; j < n - 1 - i; j++) {
+                    if (arr[j + 1] < arr[j]) {  // B G G B G
+                        // G B G G B
+                        char ch = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = ch;
+
+                    }
+                }
+            }
+
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+ " ,");
+        }
+    }
+}
