f355fe7577177c96570570a1a089ef23971b01d9
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-04-24 00:09:25 +0530

    Tit For Tat.

---
+++ TitForTat.java
@@ -0,0 +1,50 @@
+package A2OJ.Division_A;
+
+import java.util.Scanner;
+
+public class TitForTat {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- >0){
+            int n,k;
+            n = sc.nextInt();
+            k = sc.nextInt();
+            int[] arr = new int[n];
+            int[] a = new int[n];
+            for(int i=0;i<n;i++) {
+                arr[i] = sc.nextInt();
+
+            }
+            // Bubble Sorting
+            int[] ra = bubbleSort(arr, n);
+
+
+            for (int i = 0; i < n; i++) {
+                System.out.println(arr[i]+",  ");
+            }
+
+
+        }
+    }
+
+    public static int[] bubbleSort(int[] arr, int n){
+        for(int i=0;i<n;i++){
+            for(int j =0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
+
+/*
+ 2
+ 3 1
+ 3 1 4
+ 2 10
+ 1 0*/
