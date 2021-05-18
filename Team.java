bbdf03f82f991687897cd589b5b4767e4a95ceca
hrincredible007 <hritikbhardwaj2002@gmail.com>
2021-05-18 19:41:24 +0530

    Team

---
+++ Team.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Team {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        int res= 0;
+        for (int i = 0; i <n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int count = 0;
+            if(a==1){
+                count++;
+            }
+            if(b==1){
+                count++;
+            }
+            if(c==1){
+                count++;
+            }
+            if(count>1){
+                res++;
+            }
+        }
+        System.out.println(res);
+    }
+}
