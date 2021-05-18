813100df47cf1c28fb0a0647d18a2ab097edea63
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-05-18 23:10:54 +0530

    Next Round

--- NextRound.java
+++ NextRound.java
@@ -1,2 +1,31 @@
+import java.util.Scanner;
+
 public class NextRound {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+        int count = 0, flag = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i]!=0){
+                flag=1;
+            }
+        }
+        if(flag==0){
+            System.out.println(0);
+            System.exit(0);
+        }
+        int pos = arr[k-1];
+        int i = 0;
+        while(i!=n && arr[i]>=pos){
+            if(arr[i]!=0) {
+                count++;
+            }
+            i++;
+        }
+        System.out.println(count);
+
+    }
 }
