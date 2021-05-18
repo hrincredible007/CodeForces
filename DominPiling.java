52f2ffb8b32801742a900cc0252c23766325666b
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-05-18 23:09:59 +0530

    Domin Pilling

---
+++ DominPiling.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DominPiling {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int i = 1;
+        int result = (m*n)/(2);
+        System.out.println(result);
+    }
+}
