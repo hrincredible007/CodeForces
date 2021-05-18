3e2a1252d028b5ff86f8a78d124cf2b0161c36a3
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-05-18 23:19:30 +0530

    Add files via upload
    Bit Plus Plus

---
+++ BitPusPlus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BitPusPlus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+        while(n-->0){
+            String str = sc.next();
+            if(str.contains("++")){
+                x++;
+            }
+            else{
+                x--;
+            }
+        }
+        System.out.println(x);
+    }
+}
