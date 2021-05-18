cf140b8de017a5d35c804ac172dd61848fee6ea8
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-05-18 23:31:24 +0530

    Word Capitalization

---
+++ WordCapitalization.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WordCapitalization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String s = "";
+        char ch = str.charAt(0);
+        int k = (int)ch;
+        if(k>=97 && k<=122){
+            k-=32;
+        }
+        s = s+(char)k;
+        for (int i = 1; i < str.length(); i++) {
+            char ch1 = str.charAt(i);
+            s = s+ch1;
+        }
+        System.out.println(s);
+
+    }
+}
