2159eb9b452395be2cfea31d2766393fb251d175
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-06-19 22:46:36 +0530

    Bad Boy

---
+++ Codeforces/BadBoy.java
@@ -0,0 +1,18 @@
+package Contests.CodeForcesRound726;
+
+import java.util.Scanner;
+
+public class BadBoy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n, m, i, j;
+            n = sc.nextInt();
+            m = sc.nextInt();
+            i = sc.nextInt();
+            j = sc.nextInt();
+            System.out.println(1+" "+1+" "+n+" "+m);
+        }
+    }
+}
