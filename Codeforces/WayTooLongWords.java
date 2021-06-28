d650c1300b0c81240cd5f3b1a2cfadf5da1ab958
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-06-28 12:07:13 +0530

    Way Too Long Words

--- Codeforces/WayTooLongWords.java
+++ Codeforces/WayTooLongWords.java
@@ -1,7 +1,6 @@
 package A2OJ.Division_A;
 
 import java.util.Scanner;
-
 public class WayTooLongWords {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -10,7 +9,6 @@ public class WayTooLongWords {
         while(n-- >0){
             String str = sc.next();
             if(str.length()>10)
-
                 System.out.println(str.charAt(0)+""+(str.length()-2)+str.charAt(str.length()-1));
             else
                 System.out.println(str);
