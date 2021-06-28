b9abfb53171cef5ae8c00f89832f9de1859163cc
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-06-28 11:50:38 +0530

    Game With Sticks

--- Codeforces/GameWithSticks.java
+++ Codeforces/GameWithSticks.java
@@ -1,13 +1,11 @@
 package A2OJ.Division_A;
 
 import java.util.Scanner;
-
 public class GameWithSticks {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, m;
-        n = sc.nextInt();
-        m = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
         String winner = "Akshat";
         if(n==1 || m==1){
             winner = "Akshat";
