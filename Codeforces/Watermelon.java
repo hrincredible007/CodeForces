faeb85120e5b1cb9f52dd60c35da7d32091c765d
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-06-28 11:57:19 +0530

    Watermelon

--- Codeforces/Watermelon.java
+++ Codeforces/Watermelon.java
@@ -5,15 +5,11 @@ import java.util.Scanner;
 public class Watermelon {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int w;
-        w = sc.nextInt();
-
-
+        int w = sc.nextInt();
         if(w%2 == 0 && w!=2){
             System.out.println("YES");
         }
         else{
-
             System.out.println("NO");
         }
     }
