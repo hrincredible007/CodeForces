a98cab83006b9055632b942965c9432a4cae8530
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-06-28 11:56:22 +0530

    String Task

--- Codeforces/StringTask.java
+++ Codeforces/StringTask.java
@@ -1,15 +1,11 @@
 package A2OJ.Division_A;
 
 import java.util.Scanner;
-
 public class StringTask {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String vow = "aeiouyAEIOUY";
-        String str;
-
-
-        str = sc.next();
+        String str = sc.next();
         String wd ="";
         for (int i =0;i<str.length();i++){
             char ch = str.charAt(i);
