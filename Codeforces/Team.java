e60ad0366996e00e22de0ac911a677cd571bd50a
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-06-28 11:56:50 +0530

    Team

--- Codeforces/Team.java
+++ Codeforces/Team.java
@@ -3,8 +3,7 @@ import java.util.Scanner;
 public class Team {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n;
-        n = sc.nextInt();
+        int n = sc.nextInt();
         int res= 0;
         for (int i = 0; i <n; i++){
             int a = sc.nextInt();
