422f0bd3967c122a3968fb2d01ac5dd2f829a8a5
hrincredible007 <59758425+hrincredible007@users.noreply.github.com>
2021-08-03 10:28:58 +0530

    Gregor And Cryptography

---
+++ Codeforces/GregorAndCryptography.java
@@ -0,0 +1,16 @@
+package Divivsion_A;
+
+import java.util.Scanner;
+
+public class GregorAndCryptography {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            int P = sc.nextInt();
+            int res = P/2;
+            res = res*2;
+            System.out.println(2+" "+ res);
+        }
+    }
+}
